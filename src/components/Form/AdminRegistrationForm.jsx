0bc93a89e5443e4bec14523b1e554f7cb9555b18
Mouhamad-gaye <gayemouhamad280@gmail.com>
2025-06-10 10:09:46 -0600

    admin reguster commit

--- src/components/Form/AdminRegistrationForm.jsx
+++ src/components/Form/AdminRegistrationForm.jsx
@@ -1,5 +1,39 @@
-
+import { useState } from "react"
+import axios from "axios"
 
 export default function AdminRegistrationForm() {
-    return
+
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    });
+
+    const [message, setMessage] = useState("");
+    const [errors, setErrors] = useState({});
+
+    
+    return (
+        <form>
+            <h2>Admin Registration</h2>
+                <div>
+                    <label>First Name</label>
+                    <input type="text" name="firstName" value={formData.firstName}></input>
+                </div>
+                <div>
+                    <label>Last  Name</label>
+                    <input type="text" name="firstName" value={formData.lastName}></input>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="text" name="firstName" value={formData.email}></input>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="text" name="firstName" value={formData.password}></input>
+                </div>
+            
+        </form>
+    )
 }
